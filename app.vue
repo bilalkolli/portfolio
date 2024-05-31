d6b78e06f636f77e2a23c837c5ae2b48297713e4
bilalkolli <bilal.kokoko@outlook.fr>
2024-05-31 23:56:08 +0200

    ajout d'un titre et d'une description pour mon portfolio dans app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
-<template>
-  <div>
-    <NuxtPage />
-  </div>
+<template> 
+  <Head> 
+    <Title>Bilal Kolli</Title>
+    <meta name="description" content="Explorez mon portfolio de développeur web front-end et comment je peux dynamiser vos projets avec des solutions en développement et innovation !">
+  </Head>
+  
+  <NuxtLayout> 
+    <NuxtPage /> 
+  </NuxtLayout> 
 </template>
 
 <style>
-*{
-  padding: 0;
-  margin: 0;
-}
+@import './assets/global.css';
 </style>
