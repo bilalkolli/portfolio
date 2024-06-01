757d3517f54a5dd78aa29d640d5294a8c60f93e7
bilalkolli <bilal.kokoko@outlook.fr>
2024-06-01 14:24:20 +0200

    ajout d'un link preload dans le app.vue pour l'image hero et modification du format de l'image hero

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <Head> 
     <Title>Bilal Kolli</Title>
     <meta name="description" content="Explorez mon portfolio de développeur web front-end et comment je peux dynamiser vos projets avec des solutions en développement et innovation !">
+    <link rel="preload" as="image" href="photohero.webp">
   </Head>
   
   <NuxtLayout> 
